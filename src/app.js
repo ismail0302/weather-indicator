523fa8acfaba9ae0a0aea6e36799edce84b0c712
muhammad ismail <mi477048@gmail.com>
2021-03-20 23:21:22 -0700

    about text

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
-
+const port = process.env || 3000
 const app = express()
 
 // Define paths for Express config
@@ -97,6 +97,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
